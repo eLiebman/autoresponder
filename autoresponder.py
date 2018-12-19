7c1ce4d57b16355cf9becb9671863ae267d71376
Elliot Liebman <elgincheese@gmail.com>
2018-12-18 21:20:51 -0600

    Add error handling

--- autoresponder.py
+++ autoresponder.py
@@ -3,15 +3,23 @@ from googlevoice import Voice
 
 
 def run():
+    received_messages = []
+    delay_time = 7 # time in seconds to wait between message checks
+    response_message = "Hello Human! \n You will now receive the Zine Prologue. When finished, you may proceed to enter the Zine\n It works best with headphones. \n https://drive.google.com/file/d/1AKW5qbpcqbbVzaICtkejU0DBrW9HlIdb/view?usp=sharing" # your autoreply message
     voice = Voice()
     voice.login()    # login will prompt for email/password in the command line
-    response = "I'm testing a python app that sends texts through google voice. Text me back!"
+    print('Running...\nPress ctrl C to quit')
+
     while True:
         # get messages
-        for msg in voice.sms().messages:
-            if not msg.isRead:
-                voice.send_sms(msg.phoneNumber, response)
-                msg.mark(1)
-        time.sleep(30)
+        try:
+            for msg in voice.sms().messages:
+                if not msg.isRead:
+                    # reply
+                    voice.send_sms(msg.phoneNumber, response_message)
+                    msg.mark(1)
+            time.sleep(delay_time)
+        except:
+            continue
 
 __name__ == '__main__' and run()
