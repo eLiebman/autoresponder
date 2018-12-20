de1f1781ad9445f2a07c46864d5000d6dea12550
Elliot Liebman <elgincheese@gmail.com>
2018-12-19 22:56:00 -0600

    Handle login exceptions

--- autoresponder.py
+++ autoresponder.py
@@ -1,13 +1,18 @@
 import time
 from googlevoice import Voice
 
+def login(voice):
+    try:
+        voice.login()
+    except:
+        login(voice)
 
 def run():
     received_messages = []
     delay_time = 7 # time in seconds to wait between message checks
     response_message = "Hello Human! \n You will now receive the Zine Prologue. When finished, you may proceed to enter the Zine\n It works best with headphones. \n https://drive.google.com/file/d/1AKW5qbpcqbbVzaICtkejU0DBrW9HlIdb/view?usp=sharing" # your autoreply message
     voice = Voice()
-    voice.login()    # login will prompt for email/password in the command line
+    login(voice)    # login will prompt for email/password in the command line
     print('Running...\nPress ctrl C to quit')
 
     while True:
